c1cf8c1dbf0847738f840848397176e7ce6bbae9
tcloaken <trevor.enright@gmail.com>
2016-11-03 21:48:53 -0700

    ok

---
+++ CONFIG.py
@@ -0,0 +1,11 @@
+"""
+Configuration of memo app. 
+Generated Wed Nov  2 18:29:22 PDT 2016
+Edit to fit development or deployment environment.
+
+"""
+
+PORT=5000
+DEBUG = True  # Set to False for production use
+secret_key="de6a6a82b2e06e8e0ac7ab74f548d174"
+
