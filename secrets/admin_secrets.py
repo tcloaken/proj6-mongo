645d58ec574fde65dc6932bd374decc0210f67c8
tcloaken <trevor.enright@gmail.com>
2016-11-04 01:55:25 -0700

    this checks

--- secrets/admin_secrets.py
+++ secrets/admin_secrets.py
@@ -1,7 +1,7 @@
 # These secrets apply to the whole MongoDB instance and must
 # therefore be kept separate from the per-db client_secrets.py .
 #
-admin_user="bogus" 
-admin_pw="bogus"       
+admin_user="tenright" 
+admin_pw="password"       
 port="27017"            
 host="localhost"  
